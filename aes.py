33692d5d29cb01b4bc5e665e6b1fc3b45b628895
angga88888 <fingerstyle88888@gmail.com>
2019-11-17 18:37:58 +0800

    just for 16 bytes

--- aes.py
+++ aes.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
-
-
-"""
-    Copyright (C) 2012 Bo Zhu http://about.bozhu.me
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-    DEALINGS IN THE SOFTWARE.
-"""
-
 Sbox = (
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
     0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -82,7 +57,6 @@ def text2matrix(text):
             matrix.append([byte])
         else:
             matrix[i // 4].append(byte)
-    print (byte)
     return matrix
 
 
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import unittest
+import codecs
 from aes import AES
 
 class AES_TEST(unittest.TestCase):
@@ -7,18 +8,22 @@ class AES_TEST(unittest.TestCase):
         self.AES = AES(master_key)
 
     def test_encryption(self):
-        # message: my message
-        plaintext = 0x6d79206d657373616765
+        # message: my message / 0x6d79206d657373616765
+        # convert from byte string to hex int
+        message = int(codecs.encode(b"my message", "hex"), 16)
+        plaintext = message
         encrypted = self.AES.encrypt(plaintext)
         print ("enkrip: ")
         print (hex(encrypted))
         # self.assertEqual(encrypted, 0x3925841d02dc09fbdc118597196a0b32)
 
     def test_decryption(self):
-        ciphertext = 0xe883a19a08a94bdb88663e18edb00a18
+        ciphertext = 0x3a6a2477ad5f33fcb9bc1b50ea218956
         decrypted = self.AES.decrypt(ciphertext)
+        varDecrypted = format(decrypted, "x")
+        varDecrypted = bytes.fromhex(varDecrypted).decode("utf-8")
         print ("dekrip: ")
-        print (hex(decrypted))
+        print (varDecrypted)
         # self.assertEqual(decrypted, 0x3243f6a8885a308d313198a2e0370734)
 
 if __name__ == '__main__':
